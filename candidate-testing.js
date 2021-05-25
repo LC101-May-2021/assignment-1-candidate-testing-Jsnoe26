1b78d0152bb36f899bdca2dddb6a81dfa42d33f0
Jsnoe26 <78712755+Jsnoe26@users.noreply.github.com>
2021-05-25 01:35:22 -0400

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,7 +2,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "" ;
+let candidateName = "Jade" ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "What is your name"?;
 let correctAnswer = "";
@@ -10,12 +10,12 @@ let candidateAnswer = "name";
 let questions = "What is your name";
 let correctAnswers = "";
 let candidateAnswers = "name";
-
+let name = " ";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let name = "What is your name?" ;
+input.question("What is your name?");
 }
 
 function askQuestion() {
@@ -38,7 +38,7 @@ console.log("Hello" + name);
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  let candidateName = name
+  input.question("What is your name");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
