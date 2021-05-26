c018386cdf69d310875d8d3fe2dc33da43bcb5f2
Snoe Productions <>
2021-05-26 19:37:06 +0000

    Adding correct info

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,33 +2,36 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "Jade" ;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "What is your name"?;
-let correctAnswer = "candidateName";
-let candidateAnswer = "candidateName";
-let questions = "What is your name";
-let correctAnswers = "candidateName";
-let candidateAnswers = "candidateName";
-let greeting = "Welcome!";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions;
+let correctAnswers;
+let candidateAnswers;
+let greeting;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-input.question("What is your name?" + candidateName); {
-  console.log(greeting ${candidateName});
+  candidateName = input.question("What is your name? "); 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+    candidateAnswer = input.question(question);
+    //console.log(candidateAnswer);
 }
 
 function gradeQuiz(candidateAnswers) {
-console.log("Hello" + name);
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+    if(candidateAnswer === correctAnswer) {
+        console.log("Correct!");
+    } else { 
+      console.log("False!");
+    }
 
   let grade;
   
@@ -39,7 +42,7 @@ console.log("Hello" + name);
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  input.question("What is your name");
+    console.log("Hello! " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
